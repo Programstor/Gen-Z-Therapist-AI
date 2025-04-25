c4757bedcdfa94ccd1d1fd0e37e9507b667433ad
Programstor <bestprograms2@gmail.com>
2025-04-26 01:17:02 +0300

    run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,9 @@
-streamlit run app.py
+@echo off
+rem Activate the virtual environment
+call .\.venv\Scripts\activate.bat
+
+rem Run Streamlit app
+python -m streamlit run app.py
+
+rem Deactivate the virtual environment after the app finishes
+deactivate
